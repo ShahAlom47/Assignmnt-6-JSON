a558649ec650031f41292988e7b2b100a9fccdd0
sah alom <sahalom4729@gmail.com>
2024-03-03 17:48:21 +0400

    added title list function  and loadind sp...

--- JS/index.js
+++ JS/index.js
@@ -2,31 +2,61 @@
 const searchBox = document.getElementById('search')
 const searchBtn = document.getElementById('searchBtn')
 
-const rightContainer =document.getElementById('right-container');
+const loading = document.getElementById('loading');
+const listContainer =document.getElementById('list-container');
+const readCounter = document.getElementById('list-counter');
 const leftCardContainer = document.getElementById('left-card-container');
 const allPostUrl = 'https://openapi.programming-hero.com/api/retro-forum/posts'
 
 
+const title= [];
+let Loaded=false
+
+
+
+let listCount =0;
 let Active = '';
 
-fetch(allPostUrl)
+
+
+loadingShowHide(Loaded);
+setTimeout(() => {
+    fetch(allPostUrl)
     .then(res => res.json())
     .then(datas => printCard(datas.posts))
+}, 2000);
+
 
 searchBtn.addEventListener('click', () => {
+    Loaded=false
+    loadingShowHide(Loaded)
+
+    leftCardContainer.innerHTML = '';
     let searchValue = searchBox.value
     searchBox.value='';
-    const categoryPost = `https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchValue}`
-    fetch(categoryPost)
-        .then(res => res.json())
-        .then(datas => printCard(datas.posts))
+
+    setTimeout(() => {
+       
+        const categoryPost = `https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchValue}`
+        fetch(categoryPost)
+            .then(res => res.json())
+            .then(datas => printCard(datas.posts))
+        
+    }, 2000);
+
+   
 
     
 })
 
 const printCard = (datasArr) => {
     leftCardContainer.innerHTML = '';
-    rightContainer.innerHTML='';
+
+    if(datasArr.length===0){
+
+        
+    }
+ 
     for (let datas of datasArr) {
 
         if (datas.isActive) {
@@ -51,7 +81,7 @@ const printCard = (datasArr) => {
 
     </div>
     <div class="main space-y-3">
-        <h1 class=" font-bold text-xl">${datas.title}</h1>
+        <h1 id='' class=" font-bold text-xl">${datas.title}</h1>
         <p class="text-gray-600 ">${datas.description}</p>
 
     </div>
@@ -71,55 +101,53 @@ const printCard = (datasArr) => {
                 <p> <span>${datas.posted_time}</span> Min</p>
             </div>
         </div>
-        <button onclick='cc()' id="card-btn" class="rounded-full "><img src="image/btn icon.svg"
+        <button onclick='makeTitleList(${JSON.stringify(datas.id)})' class="rounded-full "><img src="image/btn icon.svg"
                 alt=""></button>
     </div>
 </div>
     
     `
-
+   
+    title.push(datas)
+    
+    Loaded=true
+    loadingShowHide(Loaded);
 
      leftCardContainer.appendChild(makeCard);
 
 
     }
 
-    const makeRightContant = document.createElement('div');
-    makeRightContant.className=`bg-[#12132D0D] rounded-xl p-3`
-    makeRightContant.innerHTML=`
-    <div id="header" class="flex justify-between">
-    <h1 class="font-medium text-lg ">Title</h1>
-    <div class="flex flex-1 justify-end">
-        <img src="image/ll2.svg" alt="">
-        <p class="flex font-medium text-gray-600">Mark as Read <span id="list-counter"
-                class="mx-2"> 0</span> </p>
+    
 
-    </div>
-</div>
-<div id="list-container" class="py-4 flex flex-col gap-2">
-    <div id="list" class="flex gap-3 justify-between items-start p-3 bg-white rounded-lg">
-        <h1>sdhasjs hsaddj dshjsdjhd shd</h1>
-        <div class="flex items-center">
-            <img src="image/tabler-icon-eye (1).svg" alt="">
-            <p id="list-item-view">123434</p>
-        </div>
+}
 
-    </div>
-    <div id="list2" class="flex gap-3 justify-between items-start p-3 bg-white rounded-lg">
-        <h1>sdhasjs hsaddj dshjsdjhd shd</h1>
-        <div class="flex items-center">
-            <img src="image/tabler-icon-eye (1).svg" alt="">
-            <p id="list-item-view">123434</p>
-        </div>
+function makeTitleList(id) {
+    let data =title.find(ietm=>ietm.id===id)
 
+    const makeList = document.createElement('div');
+    makeList.className=`flex gap-3 justify-between items-start p-3 bg-white rounded-lg`
+    makeList.innerHTML=`
+    <h1 class='font-semibold'>${data.title} </h1>
+    <div class="flex items-center">
+        <img src="image/tabler-icon-eye (1).svg" alt="">
+        <p id="list-item-view">${data.view_count}</p>
     </div>
 
-</div>
     `
-rightContainer.appendChild(makeRightContant)
+    listContainer.appendChild(makeList)
+    listCount+=1;
+    readCounter.innerText=listCount;
+
 }
 
-function cc() {
-
-    console.log(1234567);
+function loadingShowHide(Loaded){
+if(Loaded){
+    loading.classList.add('hidden')
 }
+else{
+    loading.classList.remove('hidden')
+}
+
+};
+
